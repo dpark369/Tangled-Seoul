059cb39727c50ef4e7e0e5f09f485669fee84953
Dan Park <dpark369@gmail.com>
2021-09-29 14:59:38 -0400

    Change carousel img based on orientation

---
+++ js/orientation.js
@@ -0,0 +1,13 @@
+// Changes Carousel img based on orientation
+const carouselLength = 3;
+
+for (let i = 0; i < carouselLength; i++) {}
+
+if (window.innerHeight > window.innerWidth) {
+	portrait = true;
+} else {
+	portrait = false;
+}
+
+if (portrait) {
+}
