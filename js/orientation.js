a787d1c519c8e962bf3e7bf907801d67f978624f
Dan Park <dpark369@gmail.com>
2021-09-30 13:53:04 -0400

    Changed Carousel img depending on orientation

--- js/orientation.js
+++ js/orientation.js
@@ -1,13 +1,20 @@
 // Changes Carousel img based on orientation
-const carouselLength = 3;
 
-for (let i = 0; i < carouselLength; i++) {}
+window.addEventListener('resize', function orientationHandler() {
+	let portrait;
+	if (window.innerHeight > window.innerWidth) {
+		portrait = true;
+	} else {
+		portrait = false;
+	}
+	if (portrait) {
+		let imgNL = document.querySelectorAll('.carousel-item > img');
+		let imgArray = Array.from(imgNL);
+		console.log(imgArray);
+		for (let i = 0; i < imgArray.length; i++) {
+			imgArray[i].src = `./img/shop_img/portrait${i + 1}.jpeg`;
+		}
+	}
+});
 
-if (window.innerHeight > window.innerWidth) {
-	portrait = true;
-} else {
-	portrait = false;
-}
-
-if (portrait) {
-}
+// Fix only firing once
